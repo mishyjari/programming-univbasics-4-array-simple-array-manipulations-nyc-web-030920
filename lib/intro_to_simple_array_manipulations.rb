2ffca3b428283a4c4a0ceb62f5f57688fd60567f
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-03 00:10:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,12 @@ def using_push(array,string)
   array.push(string)
 end
 
+def using_unshift(array,string)
+  array.using_unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
